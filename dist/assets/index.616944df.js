62d453dbad215ee213625e9b850be51adeb8efdf
Kelvin <421321327@qq.com>
2021-08-27 11:40:54 +0800

    打包

---
+++ dist/assets/index.616944df.js
@@ -0,0 +1,1 @@
+import{o as n,b as r}from"./vendor.8fdae214.js";const a={};a.render=function(a,e){return n(),r("div",null," canvas ")};export{a as default};
---
+++ dist/assets/index.b275bf0d.js
@@ -0,0 +1,1 @@
+import{o as r,b as e}from"./vendor.8fdae214.js";const n={};n.render=function(n,o){return r(),e("div",null," svg ")};export{n as default};
