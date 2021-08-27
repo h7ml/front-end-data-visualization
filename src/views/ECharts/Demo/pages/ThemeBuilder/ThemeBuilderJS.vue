12f21c8fc671cc955231a4ffde9eea8f21c26d0f
Kelvin <421321327@qq.com>
2021-08-27 16:00:27 +0800

    echarts 自定义主题 js版

--- src/views/ECharts/Demo/pages/ThemeBuilder/ThemeBuilderJS.vue
+++ src/views/ECharts/Demo/pages/ThemeBuilder/ThemeBuilderJS.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="chart" class="chart" ref="chartDom"></div>
+  <aside>
+    <p>自定义echarts主题说明：</p>
+    <p>1. 在Echarts官方编辑主题，并下载js资源文件。https://echarts.apache.org/zh/theme-builder.html</p>
+    <p>2. 将下载下来的主题文件(js)放到 node_modules/echarts/theme 目录下</p>
+    <p>3. 打开下载好的主题文件，搜索 “echarts.registerTheme” 即可找到主题名(可以自己修改改主题名)</p>
+    <p>4. 在vue中引入</p>
+    <p>5. 使用</p>
+
+    <p>备注：本例所使用的主题放在 src/assets/echarts/theme/vintage2.js 。本例的主题只改了官方提供的vintage主题的标题颜色</p>
+  </aside>
 </template>
 
 <script setup>
