1cb490e47575845717afe7b2945ff6cde25b70f1
Kelvin <421321327@qq.com>
2021-08-28 16:30:45 +0800

    echarts TopView布局

--- src/views/ECharts/DatavReportDev/components/TopView/TopView.vue
+++ src/views/ECharts/DatavReportDev/components/TopView/TopView.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    TopView
+  <div class="top__view">
+    <el-row :gutter="20">
+      <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="top__view__item">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="top__view__item">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="top__view__item">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="top__view__item">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -9,5 +30,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.top__view__item {
+  margin-bottom: 20px;
+}
 </style>
