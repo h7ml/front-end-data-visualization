ac992b9f4ab1c4ce27e451b16ab0c8f52b9fccef
Kelvin <421321327@qq.com>
2021-08-29 10:05:36 +0800

    echarts 年度销售额布局

--- src/views/ECharts/DatavReportDev/components/SalseView/SalseView.vue
+++ src/views/ECharts/DatavReportDev/components/SalseView/SalseView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="salse__view">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
       <template #header>
         <div class="menu__wrapper">
           <el-menu
@@ -22,20 +22,39 @@
             <el-date-picker
               class="salse__view__date__picker"
               size="small"
-              v-model="date"
+              v-model="datePicker.date"
               type="daterange"
               unlink-panels
               range-separator="至"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
-              :shortcuts="shortcuts"
+              :shortcuts="datePicker.shortcuts"
             >
             </el-date-picker>
           </div>
         </div>
       </template>
       <template #default>
-        <div>chart</div>
+        <div class="salse__view__chart__wrapper">
+          <ChartDom class="chart__dom" :options="chartOption" />
+          <div class="sales__view__list">
+            <div class="sales__view__title">排行榜</div>
+            <div class="list__item__wrapper">
+              <div class="list__item" v-for="item in rankData" :key="item.no">
+                <div
+                  :class="[
+                    'list__item__no',
+                    +item.no <= 3 ? 'top__no' : ''
+                  ]"
+                >
+                  {{item.no}}
+                </div>
+                <div class="list__item__name">{{item.name}}</div>
+                <div class="list__item__money">{{item.money}}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </el-card>
   </div>
@@ -43,6 +62,7 @@
 
 <script setup>
 import { ref, reactive } from 'vue'
+import ChartDom from './ChartDom.vue'
 
 const activeIndex = ref('1')
 
@@ -52,35 +72,130 @@ function onMenuSelect(index) {
 
 const radioSelect = ref('今日')
 
-const date = ref(null)
+const datePicker = reactive({
+  date: '',
+  shortcuts: [
+    {
+      text: '最近一周',
+      value: () => {
+        const end = new Date()
+        const start = new Date()
+        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+        return [start, end]
+      }
+    },
+    {
+      text: '最近一个月',
+      value: () => {
+        const end = new Date()
+        const start = new Date()
+        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+        return [start, end]
+      }
+    },
+    {
+      text: '最近三个月',
+      value: () => {
+        const end = new Date()
+        const start = new Date()
+        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+        return [start, end]
+      }
+    }
+  ]
+})
 
-const shortcuts = reactive([
-  {
-    text: '最近一周',
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-      return [start, end]
+const chartOption = ref({
+  title: {
+    text: '年度销售额',
+    textStyle: {
+      fontSize: 12,
+      color: "#666"
+    },
+    top: 20,
+    left: 25
+  },
+  xAxis: {
+    type: 'category',
+    data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    axisTick: {
+      alignWithLabel: true,
+      lineStyle: {
+        color: '#999'
+      }
+    },
+    axisLine: {
+      lineStyle: {
+        color: '#999'
+      }
+    },
+    axisLabel: {
+      color: '#333'
     }
   },
-  {
-    text: '最近一个月',
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-      return [start, end]
+  yAxis: {
+    axisLine: {
+      show: false
+    },
+    axisTick: {
+      show: false
+    },
+    splitLine: {
+      lineStyle: {
+        type: 'dotted',
+        color: '#eee'
+      }
     }
   },
-  {
-    text: '最近三个月',
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-      return [start, end]
+  series: [{
+    type: 'bar',
+    barWidth: '35%',
+    data: [200, 250, 300, 350, 186, 462, 452, 219, 626, 437, 515, 656],
+  }],
+  color: ['#3398DB'],
+  grid: {
+      top: 70,
+      bottom: 50,
+      left: 60,
+      right: 60
     }
+})
+
+const rankData = ref([
+  {
+    no: 1,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 2,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 3,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 4,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 5,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 6,
+    name: '麦当劳',
+    money: '323,234'
+  },
+  {
+    no: 7,
+    name: '麦当劳',
+    money: '323,234'
   }
 ])
 
@@ -115,6 +230,69 @@ const shortcuts = reactive([
 
   :deep(.salse__view__date__picker) {
     margin-left: 20px;
+  }
+}
+
+.salse__view__chart__wrapper {
+  display: flex;
+  height: 270px;
+
+  .chart__dom {
+    flex: 0 0 70%;
+    width: 70%;
+    height: 100%;
+  }
+
+  .sales__view__list {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+    .sales__view__title {
+      margin-top: 20px;
+      font-size: 12px;
+      color: #666;
+      font-weight: 500;
+    }
+
+    .list__item__wrapper {
+      margin-top: 15px;
+
+      .list__item {
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        height: 20px;
+        padding: 6px 20px 6px 0;
+
+        .list__item__no {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 20px;
+          height: 20px;
+          color: #333;
+
+          &.top__no {
+            background: #000;
+            border-radius: 50%;
+            color: #fff;
+            font-weight: 500;
+          }
+        }
+
+        .list__item__name {
+          margin-left: 10px;
+          color: #333;
+        }
+
+        .list__item__money {
+          flex: 1;
+          text-align: right;
+        }
+      }
+    }
   }
 }
 
