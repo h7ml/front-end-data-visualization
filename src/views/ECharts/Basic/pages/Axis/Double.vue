abd055bc01867d04cb212488b3818bf6c236fd4b
Kelvin <421321327@qq.com>
2021-09-05 23:20:29 +0800

    echarts双坐标系注释

--- src/views/ECharts/Basic/pages/Axis/Double.vue
+++ src/views/ECharts/Basic/pages/Axis/Double.vue
@@ -1,3 +1,4 @@
+<!-- ECharts - 双坐标系 -->
 <template>
   <div id="chart" class="chart" ref="chartDom"></div>
 </template>
@@ -5,6 +6,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import * as echarts from 'echarts'
+import { useStore } from 'vuex'
+
+const store = useStore()
 
 // echart元素
 const chartDom = ref(null)
@@ -59,6 +63,7 @@ function chartInit() {
 }
 
 onMounted(() => {
+  store.commit('setComponentPath', 'src/views/ECharts/Basic/pages/Axis/Double.vue')
   chartInit()
 })
 
