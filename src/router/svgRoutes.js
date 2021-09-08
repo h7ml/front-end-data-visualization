d9284330a0b24bb3d42d934d192233c7877fb89a
Kelvin <421321327@qq.com>
2021-09-08 22:12:25 +0800

    svg viewport and viewBox

--- src/router/svgRoutes.js
+++ src/router/svgRoutes.js
@@ -7,5 +7,28 @@ export default {
     navState: true,
     navName: 'SVG',
     icon: 'el-icon-s-management'
-  }
+  },
+  children: [
+    {
+      path: 'SVG-basic',
+      name: 'SVGBasic',
+      component: () => import('@/views/SVG/Basic/index.vue'),
+      meta: {
+        navState: true,
+        navName: '基础',
+        icon: 'el-icon-s-shop'
+      },
+      children: [
+        {
+          path: 'SVG-viewportAndViewBox',
+          name: 'SVGStated',
+          component: () => import('@/views/SVG/Basic/pages/ViewportAndViewBox/ViewportAndViewBox.vue'),
+          meta: {
+            navState: true,
+            navName: 'Viewport 和 ViewBox'
+          }
+        }
+      ]
+    }
+  ]
 }
