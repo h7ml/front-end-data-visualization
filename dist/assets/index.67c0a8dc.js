3f423dfd80ef15c34cf96e2069e37a499f457f8c
Kelvin <421321327@qq.com>
2021-09-09 17:56:02 +0800

    build

--- dist/assets/index.67c0a8dc.js
+++
@@ -1,1 +0,0 @@
-import{r,o,c as s}from"./vendor.63933c18.js";const a={};a.render=function(a,c){const e=r("router-view");return o(),s(e,{class:"main__x"})},a.__scopeId="data-v-5b064c2c";export{a as default};
--- dist/assets/index.fae4f066.js
+++
@@ -1,1 +0,0 @@
-import{o as n,b as r}from"./vendor.63933c18.js";const o={};o.render=function(o,a){return n(),r("div",null," canvas ")};export{o as default};
