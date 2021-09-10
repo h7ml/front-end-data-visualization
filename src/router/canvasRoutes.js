22d8f70a862c2cb7725c9674fa57817f21362f1f
Kelvin <421321327@qq.com>
2021-09-10 21:19:09 +0800

    canvas bubble

--- src/router/canvasRoutes.js
+++ src/router/canvasRoutes.js
@@ -7,5 +7,28 @@ export default {
     navState: true,
     navName: 'Canvas',
     icon: 'el-icon-s-management'
-  }
+  },
+  children: [
+    {
+      path: 'canvas-demo',
+      name: 'CanvasDemo',
+      component: () => import('@/views/Canvas/Demo/index.vue'),
+      meta: {
+        navState: true,
+        navName: 'Demo',
+        icon: 'el-icon-sugar'
+      },
+      children: [
+        {
+          path: 'canvas-demo-bubble',
+          name: 'CanvasDemoBubble',
+          component: () => import('@/views/Canvas/Demo/pages/Bubble/Bubble.vue'),
+          meta: {
+            navState: true,
+            navName: '泡泡'
+          }
+        }
+      ]
+    }
+  ]
 }
---
+++ src/views/Canvas/Demo/pages/Bubble/Bubble.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app__home" id="appHome">
+    <canvas id="canvas"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import bubble from './index.js'
+onMounted(() => {
+  bubble()
+})
+</script>
+
+<style lang="scss" scoped>
+.app__home {
+  width: 800px;
+  height: 800px;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
