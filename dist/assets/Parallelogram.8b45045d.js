c0732e498bcf0e8fc43f86aaecfcdcd51ae21c9d
Kelvin <421321327@qq.com>
2021-09-15 20:54:29 +0800

    build

---
+++ dist/assets/Parallelogram.8b45045d.js
@@ -0,0 +1,1 @@
+import{p as a,b as d,o as e,f as s}from"./vendor.ec101613.js";const o={};a("data-v-adad63fe");const r={class:"parallelogram"};d(),o.render=function(a,d){return e(),s("div",r,"一个元素实现，背景倾斜，文字不倾斜")},o.__scopeId="data-v-adad63fe";export{o as default};
