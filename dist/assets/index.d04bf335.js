d8a15e898f672b482e4b658f8703404735034cb7
Kelvin <421321327@qq.com>
2021-10-19 14:31:36 +0800

    build

---
+++ dist/assets/index.d04bf335.js
@@ -0,0 +1,1 @@
+import{r,o as e,c as o}from"./vendor.ec101613.js";const s={};s.render=function(s,a){const t=r("router-view");return e(),o(t,{class:"main__x"})},s.__scopeId="data-v-93cf9804";export{s as default};
