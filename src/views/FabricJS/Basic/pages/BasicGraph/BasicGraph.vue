1a82bdc3b1490031bd84ffa73880d9872ff2188d
Kelvin <421321327@qq.com>
2021-10-25 11:58:21 +0800

    fabric 补充基础图形

--- src/views/FabricJS/Basic/pages/BasicGraph/BasicGraph.vue
+++ src/views/FabricJS/Basic/pages/BasicGraph/BasicGraph.vue
@@ -9,6 +9,17 @@ import { onMounted } from 'vue'
 import { useStore } from 'vuex'
 import { fabric } from 'fabric'
 
+/**
+ * 基础图形包括
+ * Circle       圆形            http://fabricjs.com/docs/fabric.Circle.html
+ * Ellipse      椭圆形          http://fabricjs.com/docs/fabric.Ellipse.html
+ * Line         线              http://fabricjs.com/docs/fabric.Line.html
+ * Polygon      多边形          http://fabricjs.com/docs/fabric.Polygon.html
+ * Polyline     折线（多段线）   http://fabricjs.com/docs/fabric.Polyline.html
+ * Rect         矩形            http://fabricjs.com/docs/fabric.Rect.html
+ * Triangle     三角形          http://fabricjs.com/docs/fabric.Triangle.html
+ */
+
 const store = useStore()
 
 function init() {
@@ -29,7 +40,9 @@ function init() {
     radius: 50,
     fill: 'green',
     left: 200,
-    top: 200
+    top: 200,
+    controls: false, // 不可编辑
+    hasControls: false, // 控件将不显示，并且不能用于操作对象
   })
 
   // 三角形
@@ -42,6 +55,47 @@ function init() {
   })
 
   canvas.add(circle, triangle) // 一次添加2个图形
+
+  // 椭圆
+  let ellipse = new fabric.Ellipse({
+    top: 20,
+    left: 20,
+    rx: 70,
+    ry: 30,
+    fill: '#f38181'
+  })
+  canvas.add(ellipse)
+
+  // 线
+  let line = new fabric.Line([450, 10, 500, 300], {
+    stroke: '#5E2300',//笔触颜色
+  })
+  canvas.add(line)
+
+  // 多边形，会自动闭合
+  let polygon = new fabric.Polygon([
+    {x: 100, y: 300},
+    {x: 300, y: 320},
+    {x: 10, y: 500},
+    {x: 40, y: 200}
+  ], {
+    fill: '#ffd3b6'
+  })
+  canvas.add(polygon)
+
+  // 折线
+  let polyline = new fabric.Polyline([
+    {x: 430, y: 430},
+    {x: 530, y: 430},
+    {x: 430, y: 530},
+    {x: 530, y: 530}
+  ], {
+    fill: 'transparent',
+    stroke: '#6639a6',
+    strokeWidth: 10
+  })
+  canvas.add(polyline)
+
 }
 
 onMounted(() => {
