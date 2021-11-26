76a4a029fd13d37262cbc8f1404f992ecf6c56fa
Kelvin <421321327@qq.com>
2021-11-26 16:08:57 +0800

    loading 6

--- src/views/Native/PureCSS/pages/Loading/Loading.vue
+++ src/views/Native/PureCSS/pages/Loading/Loading.vue
@@ -10,9 +10,12 @@
       <L3 />
       <L4 />
       <L5 />
+      <L6 />
     </div>
   </div>
 </template>
+
+<!-- 下面的代码都是组件引入和布局相关，和Loading无关的 -->
 
 <script setup>
 import { useStore } from 'vuex'
@@ -21,6 +24,7 @@ import L2 from './components/L2.vue'
 import L3 from './components/L3.vue'
 import L4 from './components/L4.vue'
 import L5 from './components/L5.vue'
+import L6 from './components/L6.vue'
 
 const store = useStore()
 store.commit('setComponentPath', 'src/views/CSS/Pure/pages/Loading/Loading.vue')
@@ -33,21 +37,31 @@ store.commit('setComponentPath', 'src/views/CSS/Pure/pages/Loading/Loading.vue')
 }
 
 // 布局用的，这段和loading无关
-.com__box {
-  position: relative;
+::v-deep .com__box {
+  display: block;
   width: 100px;
   height: 100px;
   border: 1px solid #ccc;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
   margin: 10px;
+  padding: 10px;
+  box-sizing: border-box;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  ::v-deep h4.explain {
+  .loading {
+    margin-top: -15px;
+  }
+
+  h4.explain {
     margin-top: 20px;
     margin-bottom: 0;
     font-size: 14px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
   }
 }
 </style>
