0683a638d2fec25a52548f9edbe7d351aa963458
Kelvin <421321327@qq.com>
2021-11-26 15:44:59 +0800

    删除loading 1 的兼容性写法

--- src/views/Native/PureCSS/pages/Loading/components/L1.vue
+++ src/views/Native/PureCSS/pages/Loading/components/L1.vue
@@ -16,24 +16,15 @@
   border-top-color: transparent;
   border-radius: 100%;
 
-  -webkit-animation: circle infinite 0.75s linear;
-  -moz-animation: circle infinite 0.75s linear;
-  -o-animation: circle infinite 0.75s linear;
   animation: circle infinite 0.75s linear;
 }
 
 // 转转转动画
 @keyframes circle {
   0% {
-    -webkit-transform: rotate(0);
-    -ms-transform: rotate(0);
-    -o-transform: rotate(0);
     transform: rotate(0);
   }
   100% {
-    -webkit-transform: rotate(360deg);
-    -ms-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
     transform: rotate(360deg);
   }
 }
