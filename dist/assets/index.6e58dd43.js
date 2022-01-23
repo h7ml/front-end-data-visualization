6b302898a8cd1d259ae65bb4440f13ef2ad9fbb7
Kelvin <421321327@qq.com>
2022-01-23 22:32:05 +0800

    build

---
+++ dist/assets/index.6e58dd43.js
@@ -0,0 +1,1 @@
+import{_}from"./index.a64f84be.js";import{r as o,o as r,c as n}from"./vendor.72e7bd49.js";const t={};function s(a,c){const e=o("router-view");return r(),n(e,{class:"main__x"})}var p=_(t,[["render",s],["__scopeId","data-v-58779578"]]);export{p as default};
--- dist/assets/index.f628796c.js
+++
@@ -1,1 +0,0 @@
-import{r,o as a,c as o}from"./vendor.ab550d3c.js";const s={};s.render=function(s,e){const t=r("router-view");return a(),o(t,{class:"main__x"})},s.__scopeId="data-v-93cf9804";export{s as default};
