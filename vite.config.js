3e73d3c3b16a2449fc49db4d1e26ab794448f19c
h7ml <h7ml@qq.com>
2022-06-17 12:13:15 +0800

    deploy: path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/front-end-data-visualization/', // 部署到码云的仓库名，部署时用到
-  plugins: [vue()],
+  base: '/',
   server: { // https://cn.vitejs.dev/config/#server-options
     host: '0.0.0.0'
   },
